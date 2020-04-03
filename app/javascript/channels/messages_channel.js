ac29e09fab02e342a9f1dd88d556af23af0feb7a
Bradyn Glines <glinesbdev@gmail.com>
2020-04-03 17:11:01 -0500

    Styling!

--- app/javascript/channels/messages_channel.js
+++ app/javascript/channels/messages_channel.js
@@ -1,13 +1,25 @@
 import consumer from './consumer'
 
 function submitMessages() {
+  let timer;
+
   $('#message_content').on('keydown', function (e) {
     if (e.key === 'Enter') {
       $('input').click();
       e.target.value = '';
       e.preventDefault();
+
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        const container = $('.message-container');
+        container.scrollTop(container[0].scrollHeight - container[0].clientHeight);
+      }, 50);
     }
   });
+}
+
+function htmlEntities(str) {
+  return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
 $(document).on('turbolinks:load', function () {
@@ -24,6 +36,15 @@ consumer.subscriptions.create('MessagesChannel', {
   },
 
   received(data) {
-    $(`<li>${data.content} - ${data.user}</li>`).appendTo($('#messages'));
+    $(`
+      <div class="message d-flex flex-column list-group-item">
+        <span class="mb-3">
+          ${data.user}
+        </span>
+        <span>
+          ${htmlEntities(data.content)}
+        </span>
+      </div>
+    `).appendTo($('#messages'));
   }
 });
