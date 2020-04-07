80172580f3e959c9529c4c8dfaf0374adac8d152
Bradyn Glines <glinesbdev@gmail.com>
2020-04-07 14:07:43 -0500

    Don't delete users with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
 Message.destroy_all
 Chatroom.destroy_all
 
